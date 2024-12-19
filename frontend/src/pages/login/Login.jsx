8f3cc9aa2cd9736b330dd9283f5002e7e75c0cf3
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-19 14:06:50 -0800

    added AuthContextProvider

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Login.css";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email.current.value);
+    console.log(password.current.value);
+  }
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,14 +19,14 @@ export default function Login() {
           <p className="loginDesc"> SNSを作って理解しよう</p>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
+          <form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
             <p className="loginMsg">ログインはこちら</p>
-            <input type="text" className="loginInput" placeholder="Eメール" />
-            <input type="text" className="loginInput" placeholder="パスワード" />
+            <input type="email" className="loginInput" placeholder="Eメール" required ref={email} />
+            <input type="password" className="loginInput" placeholder="パスワード" required min="6" ref={password} />
             <button className="loginButton">ログイン</button>
             <span className="loginForgot">パスワードを忘れた方へ</span>
             <button className="loginRegisterButton">アカウントを作成</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
